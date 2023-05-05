4a73cabae8acdf659842cc4da950fb3b91ac6348
Alexander <alexander.barbadyn@gmail.com>
2023-05-05 16:10:39 +0200

    fix import and add docker and docker-compose files

--- migrations/env.py
+++ migrations/env.py
@@ -21,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.url_shortener.models import ShortUrl
+from apps.url_shortener.models import ShortUrl
 target_metadata = ShortUrl.metadata
 
 # other values from the config, defined by the needs of env.py,
